8f8c4a2742932437d6049881527ccd0a0dcd603a
Phimc <41496326+Phimc@users.noreply.github.com>
2021-11-09 21:54:38 +0800

    Update Constantnums

--- ATOMS_init.py
+++ ATOMS_init.py
@@ -1,13 +1,30 @@
 #"""分配初始位置(均匀)"""
+from numpy import random as rd
 import Constantnums as C
+import math 
+v_mean = (8*C.R*C.T/C.Pi/C.M)**(1/2)
+def randangle():
+    angle = [math.cos(rd.rand()*C.Pi),math.cos(rd.rand()*C.Pi),math.cos(rd.rand()*C.Pi)]
+    return angle
 def ATOMS_INIT(ATOMS,n_gas):
     i = 0
-    for x in range():
-        for y in range():
-            for z in range():
+    for x in range(10):
+        for y in range(10):
+            for z in range(10):
                 ATOMS[i].r[0] = x*C.l/n_gas**(1/3)
                 ATOMS[i].r[1] = y*C.l/n_gas**(1/3)
                 ATOMS[i].r[2] = z*C.l/n_gas**(1/3)
+                
+                v = randspeed()
+                v_angle = randangle()
+                ATOMS[i].rv[0] = v_angle[0]*v
+                ATOMS[i].rv[1] = v_angle[1]*v
+                ATOMS[i].rv[2] = v_angle[2]*v
+
                 i += 1
-    #"""分配初始速度"""
-    v_mean = 1
+                if i > n_gas:
+                    return 0
+#"""分配初始速度"""
+def randspeed():
+    v = v_mean*rd.rand()*2
+    return v
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ n_gas = 1000
 #"""生成粒子"""
 ATOMS = [atom.atom() for i in range(n_gas)] #"""气体分子数"""
 ATOMS_init.ATOMS_INIT(ATOMS,n_gas)
+
 while True:
     print(ATOMS[10].r)
+    print(ATOMS[0].rv)
     break
