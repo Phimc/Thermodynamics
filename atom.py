767756727a36fadf4752c0d886766af158b6d3c9
Phimc <41496326+Phimc@users.noreply.github.com>
2021-11-11 23:16:43 +0800

    Update atom

--- atom.py
+++ atom.py
@@ -1,3 +1,4 @@
+import math
 import Constantnums as C
 class atom():
     def __init__(self):
@@ -9,4 +10,18 @@ class atom():
     def fresh(self):
         for i in range(3):
             self.r[i] += self.rv[i]*C.dt
+            self.rv[i] += self.ra[i]*C.dt
     
+    def force(self,atom):
+        r = math.sqrt((self.r[0]-atom.r[0])**2+(self.r[1]-atom.r[1])**2+(self.r[2]-atom.r[2])**2)
+        F = 12*C.epsilon/C.sigma((C.sigma/r)**13-(C.sigma/r)**7)
+        a = [
+        self.r[0]-atom.r[0]/r/self.m,
+        self.r[1]-atom.r[1]/r/self.m,
+        self.r[2]-atom.r[2]/r/self.m] 
+"""
+def collipsion(ATOMS):
+    for atomi in ATOMS:
+        for atomj in ATOMS:
+            if atom_
+            """
