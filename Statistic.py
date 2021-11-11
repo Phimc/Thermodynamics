ff6f2c4c6628d0b051ea14f560fe242534ea7334
Phimc <41496326+Phimc@users.noreply.github.com>
2021-11-11 10:37:22 +0800

    Movement

--- Statistic.py
+++ Statistic.py
@@ -1,8 +1,10 @@
 import math
 import matplotlib.pyplot as plt
+
 def v(atom):
     v = math.sqrt(atom.rv[0]**2+atom.rv[1]**2+atom.rv[2]**2)
     return v
+
 def V_distribution(ATOMS):
     v_diction={}
     for atom in ATOMS:
@@ -24,3 +26,18 @@ def draw(diction):
     plt.show()
     return 0
 """
+
+def atom_r(ATOMS):
+    x = []
+    y = []
+    z = []
+    for atom in ATOMS:
+        x.append(atom.r[0])
+        y.append(atom.r[1])
+        z.append(atom.r[2])
+    ax = plt.subplot(projection = '3d')
+    ax.scatter(x,y,z,c="b")
+    ax.set_xlabel('X')  # 设置x坐标轴
+    ax.set_ylabel('Y')  # 设置y坐标轴
+    ax.set_zlabel('Z')
+    plt.show()
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import atom
 import ATOMS_init
 import Statistic as Stat
+import Constantnums as C
 n_gas = 1000
 #"""生成粒子"""
 ATOMS = [atom.atom() for i in range(n_gas)] #"""气体分子数"""
 ATOMS_init.ATOMS_INIT(ATOMS,n_gas)
-
+t=0
 while True:
     print(ATOMS[102].r)
     print(ATOMS[0].rv)
+    Stat.atom_r(ATOMS)
     #p = Stat.V_distribution(ATOMS)
     #Stat.draw(p)
-    break
+    for atom in ATOMS:
+        atom.fresh()
+    t += C.dt
+    if t // 1E-6:
+        Stat.atom_r(ATOMS)
+        print(t)
+        if t == 5E-6:
+            break
